9a69caf71f9564b3f55e82f2233395a24099ec01
Aradhya <98498041+aradhyas8@users.noreply.github.com>
2022-02-18 17:22:26 -0500

    Login and JS

--- ForTheHorsesApp/views/userlogin.js
+++ ForTheHorsesApp/views/userlogin.js
@@ -1,7 +1,35 @@
 //function for login
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs'); // set the view engine to ejs
 
+app.get('/', function(req, res) {res.render('pages/index');}); // index page 
+app.get('/about', function(req, res) {    res.render('pages/about');}); // about page 
+
+app.listen(8080);
+console.log('Portal is listening to port 8080 ');
+
+
+
+
+var objUser=[{
+    username:"ethan",
+    password:"test"
+},
+{
+    username:"jonathan",
+    password:"test"
+}
+]
 function getInfo(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    console.log("Your username is:"+username+"And password is:"+password)
+    for(i=0; i<objUser.length; i++)
+    {
+        if(username==objUser[i].username && password==objUser.password){
+            console.log(username+"is logged in")
+            return
+        }
+    }
+    console.log("incorrect username and password");
 }
