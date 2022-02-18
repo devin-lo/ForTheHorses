129ef64346d8485ce5ccfda4ccf7322ea2abcc10
Jonathan Lee <jonlee50@hotmail.com>
2022-02-18 15:20:58 -0500

    user and org login pages
    Added routing to user and org pages

--- ForTheHorsesApp/app.js
+++ ForTheHorsesApp/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var userLoginRouter = require('./routes/userLogin');
+var orgLoginRouter = require('./routes/orgLogin');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/userLogin', userLoginRouter);
+app.use('/orgLogin', orgLoginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
