25a6b8bdf24f62737afad28867ee5c1ea9a65903
Aradhya <98498041+aradhyas8@users.noreply.github.com>
2022-02-19 02:55:31 -0500

    Pets button

--- ForTheHorsesApp/routes/pets.js
+++ ForTheHorsesApp/routes/pets.js
@@ -1,6 +1,30 @@
+
 var express = require('express');
 var router = express.Router();
-
+('button.load').onclick = function(){
+  var request;
+  if(window.XMLHttpRequest){
+    request = new XMLHttpRequest();
+  }
+  else {
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  request.open('GET', 'pets1.json');
+  request.onreadystagechange = function(){
+    
+      if((request.readyState === 4)&&(request.status === 200)){
+        var items = JSON.parse(request.responseText);
+        console.log(items);
+        var output = "<ul>";
+        for(var key in items){
+          output += "<li>" + items[key].bio + "</li>";
+        }
+        output += "</ul>";
+        document.getElementById("update").innerHTML = output;
+      };
+       request.send();
+  }
+}
 /* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
